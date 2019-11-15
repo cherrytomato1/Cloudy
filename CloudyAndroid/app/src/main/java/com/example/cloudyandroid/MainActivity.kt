4e56575840c3a5666152afc42f037b2c5cd3987d
cherrytomato1 <49678555+cherrytomato1@users.noreply.github.com>
2019-11-15 21:51:46 +0900

    client socket app

--- CloudyAndroid/app/src/main/java/com/example/cloudyandroid/MainActivity.kt
+++ CloudyAndroid/app/src/main/java/com/example/cloudyandroid/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.InputStream
+import java.io.OutputStream
+import java.lang.Exception
+import java.net.InetAddress
 import java.net.Socket
 
 
@@ -20,24 +24,39 @@ class MainActivity : AppCompatActivity() {
         var etIP = findViewById<EditText>(R.id.etIp)
         var sendMsg = "t".toByteArray()
 
-        var hostIP ="test"
+        //호스트 주소 선언 및 임시 주소 할당 (string)
+        var addr ="172.16.106.95"
+        // 포트
         val PORT = 8800
 
+        //버튼 클릭 리스너
         btnSend.setOnClickListener {
-
 
             //hostIP = Integer.parseInt(etIP.text.toString())
 
-            hostIP = etIP.text.toString()
+            //addr에 에딧 텍스트 값 받기
+            addr  = etIP.text.toString()
+
+            //inetAddress 형으로 받기
+            var hostIP = InetAddress.getByName(addr)
 
 
-            Toast.makeText(this,hostIP,Toast.LENGTH_LONG).show()
-            val sock = Socket(hostIP,PORT)
 
-            val outStream = sock.getOutputStream()
-            val inStream = sock.getInputStream()
+            try{
+                val sock : Socket = Socket(hostIP,PORT)
 
-            
+                val outStream : OutputStream = sock.getOutputStream()
+                val inStream : InputStream = sock.getInputStream()
+
+                Toast.makeText(this,addr,Toast.LENGTH_LONG).show()
+
+                outStream.write(sendMsg)
+
+                sock.close()
+            }
+            catch (e:Exception){
+
+            }
 
 
         }
