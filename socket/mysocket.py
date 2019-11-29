ed431031f72046f60fa2b4dab7674744bdae5456
dotori <rlagyrnjs12@naver.com>
2019-11-29 16:52:46 +0900

    소켓통신 파이썬 코드

---
+++ socket/mysocket.py
@@ -0,0 +1,42 @@
+import socket
+
+HOST = "192.168.0.8"
+PORT = 8080
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print ('Socket created')
+s.bind((HOST, PORT))
+print ('Socket bind complete')
+s.listen(1)
+print ('Socket now listening')
+
+def do_some_stuffs_with_input(input_string):
+    if input_string == "1":
+        input_string = "FORWARD"
+    elif input_string == "2":
+        input_string = "BACKWARD"
+    elif input_string == "3":
+        input_string = "LEFT"
+    elif input_string == "4":
+        input_string = "RIGHT"
+    else :
+        input_string = input_string + " ??"
+    return input_string
+
+while True:
+
+    conn, addr = s.accept()
+    print("Connected by ", addr)
+
+    while True:
+        data = conn.recv(1024)
+        data = data.decode("utf8").strip()
+        if not data: break
+        print("1: FORWARD, 2:BACKWARD, 3:LEFT, 4:RIGHT")
+        print("Received: " + data)
+        
+        res = do_some_stuffs_with_input(data)
+        print("pi :" + res)
+        conn.sendall(res.encode("utf-8"))
+
+    conn.close()
+s.close()
