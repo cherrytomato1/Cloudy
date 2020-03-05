17e5084c8566c1668b88428c45e1ad2bc7272af8
cherrytomato1 <49678555+cherrytomato1@users.noreply.github.com>
2020-03-05 11:12:54 +0900

    setDir v0.2

--- Infobot/infobot.py
+++ Infobot/infobot.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import os
-import socket               # sockect 모듈 import
 import setDir
 import RPi.GPIO as GPIO     # GPIO 모듈 import
+import threading
 from time import sleep      # time 모듈의 sleep() 함수 사용
 from motor import *           # car 모듈의 모든 함수 사용
 
@@ -20,9 +20,10 @@ GPIO.setwarnings(False)     # 오류 방지
 print ('HI')                              # 소켓 리스닝 시 출력문
 
 while True:                                                 # 무한 루프
-        
-        drct
-        print("Received: " + drct)                          # 받은 데이터 출력
+
+#        th=threading.Thread(target=setDir, args=())
+#        th.start()
+        drct=setDir()
         
         setcar(drct)                                         # 변환된 데이터에 따라 모터 동작
             
