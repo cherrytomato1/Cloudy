5d66bfa36d1a970ba83bcf0ce1a937d7347c1a16
cherrytomato1 <terec@naver.com>
2020-06-15 14:42:58 +0900

    obj_dtct module returns pos, size

--- obj_detection/obj_detection.py
+++ obj_detection/obj_detection.py
@@ -47,60 +47,67 @@ fps = FPS().start()
 
 # loop over the frames from the video stream
 def obj_dtct():
-    while True:
-        # grab the frame from the threaded video stream and resize it
-        # to have a maximum width of 400 pixels
-        frame = vs.read()
-        frame = imutils.resize(frame, width=400)
 
-        # grab the frame dimensions and convert it to a blob
-        (h, w) = frame.shape[:2]
-        blob = cv2.dnn.blobFromImage(frame, 0.007843, (300, 300), 127.5)
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
 
-        # pass the blob through the network and obtain the detections and
-        # predictions
-        net.setInput(blob)
-        detections = net.forward()
+    # grab the frame dimensions and convert it to a blob
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 0.007843, (300, 300), 127.5)
 
-        # loop over the detections
-        for i in np.arange(0, detections.shape[2]):
-            # extract the confidence (i.e., probability) associated with
-            # the prediction
-            confidence = detections[0, 0, i, 2]
+    # pass the blob through the network and obtain the detections and
+    # predictions
+    net.setInput(blob)
+    detections = net.forward()
+    
+    (startX, startY, endX, endY) = (-100,-100,-100,-100)
 
-            # filter out weak detections by ensuring the `confidence` is
-            # greater than the minimum confidence
-            if confidence > args["confidence"]:
-                # extract the index of the class label from the
-                # `detections`, then compute the (x, y)-coordinates of
-                # the bounding box for the object
-                idx = int(detections[0, 0, i, 1])
+    # loop over the detections
+    for i in np.arange(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the prediction
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the `confidence` is
+        # greater than the minimum confidence
+        if confidence > args["confidence"]:
+            
+            
+            # extract the index of the class label from the
+            # `detections`, then compute the (x, y)-coordinates of
+            # the bounding box for the object
+            idx = int(detections[0, 0, i, 1])
+            if CLASSES[idx]=="dog" :
                 box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
                 (startX, startY, endX, endY) = box.astype("int")
 
                 # draw the prediction on the frame
-                if CLASSES[idx]=="dog" :
-                                label = "{}: {:.2f}%".format(CLASSES[idx],
-                                        confidence * 100)
-                                cv2.rectangle(frame, (startX, startY), (endX, endY),
-                                        COLORS[idx], 2)
-                                y = startY - 15 if startY - 15 > 15 else startY + 15
-                                cv2.putText(frame, label, (startX, y),
-                                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-                                print("{}: {:.2f}% center x, boxsize = ( ".format(CLASSES[idx],confidence*100)+str((startX+endX)/2-200)+", "+str(endX-startX))
+            
+                label = "{}: {:.2f}%".format(CLASSES[idx],
+                        confidence * 100)
+                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                        COLORS[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(frame, label, (startX, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+                #print("{}: {:.2f}% center x, boxsize = ( ".format(CLASSES[idx],confidence*100)+str((startX+endX)/2-200)+", "+str(endX-startX))
+            
 
-        # show the output frame"
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
+    # show the output frame"
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
 
-        # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
-            break
+    # if the `q` key was pressed, break from the loop
+    #if key == ord("q"):
+    #    break
 
-        # update the FPS counter
-        fps.update()
-        #print("dtct running ...")
-
+    # update the FPS counter
+    fps.update()
+    #print("dtct running ...")
+    return ((startX+endX)/2-200),(endX-startX)
+    #return 1,2
 # stop the timer and display FPS information
 def stop_dtct():
     fps.stop()
--- obj_detection/test.py
+++ obj_detection/test.py
@@ -1,5 +1,8 @@
 import obj_detection as dtct
+#import cloudy as cldy
+while True :
+    x, size = dtct.obj_dtct()
 
-dtct.obj_dtct()
+    print(x,size)
 dtct.stop_dtct()
 
