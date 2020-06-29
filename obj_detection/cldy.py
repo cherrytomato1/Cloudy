c0a3d62977207153de40a0cf3456f123525985da
cherrytomato1 <terec@naver.com>
2020-06-29 21:42:34 +0900

    adding source synthesis

--- obj_detection/cldy.py
+++ obj_detection/cldy.py
@@ -43,16 +43,16 @@ sw.daemon =True
 print("START!! ")
 
 
-#print ('Socket created')                                    # 소켓 생성 시 출력문
-#s.bind((HOST, PORT))                                        # 소켓 주소 정보 할당
-#print ('Socket bind complete')                              # 소켓 바인드 성공 시 출력문
-#s.listen(1)                                                 # 소켓 연결 수신 대기 상태
-#print ('Socket now listening')                              # 소켓 리스닝 시 출력문
-#print ('IP address:' + HOST)    
+print ('Socket created')                                    # 소켓 생성 시 출력문
+s.bind((HOST, PORT))                                        # 소켓 주소 정보 할당
+print ('Socket bind complete')                              # 소켓 바인드 성공 시 출력문
+s.listen(1)                                                 # 소켓 연결 수신 대기 상태
+print ('Socket now listening')                              # 소켓 리스닝 시 출력문
+print ('IP address:' + HOST)    
 while True:                                                 # 무한 루프
-    #conn, addr = s.accept()                                 # Client에서 연결 요청이 들어올 경우 연결 수락
-    #print("Connected by ", addr)                            # 연결된 Client의 addr 출력         
-    #rs.start()
+    conn, addr = s.accept()                                 # Client에서 연결 요청이 들어올 경우 연결 수락
+    print("Connected by ", addr)                            # 연결된 Client의 addr 출력         
+    rs.start()
     #sw.start()
 
     while True:
@@ -61,11 +61,8 @@ while True:                                                 # 무한 루프
         #state = state.decode("utf8").strip()
         #if not state: break                                  # 데이터 수신이 안되는 경우 무한 루프를 벗어남
 
-        #state = '1'
-        #state = state[len(state) - 1]
         
-        state = 1
-        pos, size = dtct.obj_dtct()
+        state = state[len(state) - 1]
         
         pos= int(pos) + 500
         if(size>200) :
@@ -75,7 +72,10 @@ while True:                                                 # 무한 루프
         
         print(str(state) + ', ' + str(size) +', '+str(pos))
 
-        data = str(state)+str(size)+str(pos)
+        if(state = '7') :
+            data = '1'+str(size)+str(pos)
+        else
+            data = str(state)
         
         
     conn.close()                                            # 연결 끊기
